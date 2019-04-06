f38127c96f5c5a4e4665e3a8b1f0280f03888c79
Melissa Delgado Infante <melissadelgadoinfante@Melissas-MacBook-Pro.local>
2019-04-06 14:13:36 +0200

    user form <3

--- controllers/places.controller.js
+++ controllers/places.controller.js
@@ -1,2 +1,24 @@
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+const Place = require('../models/places.model');
+
+module.exports.list = (req, res, next) => {
+  const criteria = {};
+
+  if (req.query.place) {
+    criteria.place = new RegExp(req.query.place, 'i');
+  }
+
+  Place.find(criteria)
+    .then(places => res.render('places/list', { 
+      places,
+      place: req.query.place 
+    }))
+    .catch(error => next(error));
+}
 
 
+module.exports.create = (req, res, next) => {
+  console.log('hello')
+  res.render('places/form');
+}
--- routes/places.routes.js
+++ routes/places.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const placesController =  require('../controllers/places.controller');
 
+router.get('/new', placesController.create);
+// router.post('/', placesController.doCreate);
 
+// router.get('/', placesController.list);
+// router.get('/:id', placesController.details);
 
 module.exports = router;
