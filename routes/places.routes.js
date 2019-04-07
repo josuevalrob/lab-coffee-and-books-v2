1cb62e9853f16cf9114843d4de2e23343dfbb73f
Josue Valencia <josue.valencia@domo.ec>
2019-04-07 12:34:53 +0200

    theming place list.

--- routes/places.routes.js
+++ routes/places.routes.js
@@ -5,7 +5,9 @@ const placesController =  require('../controllers/places.controller');
 router.get('/new', placesController.create);
 router.post('/', placesController.doCreate);
 
+// list.hbs && singlePlace
 router.get('/', placesController.list);
+// details.hbs
 router.get('/:id', placesController.details);
 
 module.exports = router;
