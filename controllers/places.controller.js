2543456a7219e6c163da4757f62e948e6b5a34e3
Josue Valencia <josue.valencia@domo.ec>
2019-04-07 12:13:57 +0200

    list with some real data...

--- controllers/places.controller.js
+++ controllers/places.controller.js
@@ -4,17 +4,13 @@ const Place = require('../models/places.model');
 
 module.exports.list = (req, res, next) => {
   // const criteria = {};
-  res.render('places/list')
   // if (req.query.place) {
   //   criteria.place = new RegExp(req.query.place, 'i');
   // }
 
-  // Place.find(criteria)
-  //   .then(places => res.render('places/list', { 
-  //     places,
-  //     place: req.query.place 
-  //   }))
-  //   .catch(error => next(error));
+  Place.find()
+    .then(places => res.render('places/list', {places}))
+    .catch(error => next(error));
 }
 
 
