bd672cdd58f2615b4afe8666345522b65cfca7ba
Josue Valencia <josue.valencia@domo.ec>
2019-04-07 13:18:11 +0200

    helper for edit working,

--- helpers/places.helper.js
+++ helpers/places.helper.js
@@ -1,7 +1,8 @@
 const hbs = require('hbs');
 
-hbs.registerHelper('isMainCategory', function (book, category, options) {
-  if (book.mainCategory === category) {
+hbs.registerHelper('isMainCategory', function (place, category, options) {
+  console.log(category)
+  if (place.type.includes(category)) {
     return options.fn(this)
   } else {
     return options.inverse(this)
